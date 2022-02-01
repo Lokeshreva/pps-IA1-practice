2b6baa4da06a8820f22569b2bb0db80bb890df59
Lokeshreva <94587558+Lokeshreva@users.noreply.github.com>
2022-02-01 11:09:42 +0000

    p8 original

--- p8final.c
+++ p8final.c
@@ -1,35 +1,69 @@
 #include<stdio.h>
-float input()
+typedef struct complex
 {
-  float n;
-  printf("enter a number=\n");
-  scanf("%f",&n);
-  return n;
+float real;
+float img;
+}complex;
+int get_n()
+{
+int n;printf("enter the number of complex numbers :");
+scanf("%d",&n);
+return n;
 }
-float my_sqrt(float n)
+complex input_complex()
 {
-  float sqrt_result;
-  sqrt_result=n/2;
-  float root=0;
-  while(sqrt_result!=root)
-  {
-   root=sqrt_result;
-   sqrt_result=(n/root+root)/2;
-  }
-  return sqrt_result;
+complex c;
+printf("enter the real part:");
+scanf("%f",&c.real);
+printf("enter any imaginary part :");
+scanf("%f",&c.img);
+return c;
 }
-void output(float n, float sqrt_result)
+void input_n_complex(int n,complex c[n])
 {
-  int j,k;
-  j=n;
-  k=sqrt_result;
-  printf("square root of %d=%d\n",j,k);
+for(int i=0;i<n;i++)
+{
+c[i]=input_complex();
+}
+}
+complex add(complex a,complex b)
+{
+complex res;
+res.real=a.real+b.real;
+res.img=a.img+b.img;
+return res;
+}
+complex add_n_complex(int n,complex c[n])
+{
+complex res;
+res.img=0;
+res.real=0;
+for(int i=0;i<n;i++)
+{
+res=add(res,c[i]);
+}
+return res;
+}
+void output(int n,complex c[n],complex result)
+{
+if(n==1)
+{
+printf("%0.2f+%0.2fi is %0.2f+%0.2fi",c[0].real,c[0].img,result.real,result.img);
+}
+else{
+for(int i=0;i<n;i++)
+{
+printf("%0.2f+%0.2fi",c[i].real,c[i].img);
+}
+printf("is   %0.2f+%0.2fi",result.real,result.img);
+}
 }
 int main()
 {
-  float n,sqrt_result;
-  n=input();
-  sqrt_result=my_sqrt(n);
-  output(n,sqrt_result);
-  return 0;
-} 
+int n=get_n();
+complex c[n],res;
+input_n_complex(n,c);
+res=add_n_complex(n,c);
+output(n,c,res);
+return 0;
+}
